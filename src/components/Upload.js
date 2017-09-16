8287d2b804a8d14cb0f438e9264410a845791a6d
Andrew Bresee <andrew.bresee@smartthings.com>
2017-09-15 18:39:13 -0700

    Add upload preview to upload page and fix problem with navigation
    This patch adds upload functionality to the upload page. It
    allows the client to preview an image. This will later be used to
    save images to the Picasa web albumb.
    
    This patch also fixes a bug that was introduced with retrieving
    session on the navigation component.

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -3,10 +3,26 @@ import React from 'react';
 import Navigation from './Navigation';
 
 export default class Upload extends React.Component {
+  previewFile (event) {
+    const file = event.target.files[0]
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      let targetElement = this.refs.imgPreview
+      targetElement.src = reader.result
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Upload your photos here</h2>
+        <input type="file" onChange={this.previewFile.bind(this)}/>
+        <img src="" ref="imgPreview"/>
       </div>
     );
   }
