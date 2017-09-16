404fa5c886f08a9ae3e04d7feefdcb964c7a755b
Andrew Bresee <andrew.bresee@smartthings.com>
2017-09-16 13:53:32 -0700

    Setup initial authorization for Google Photo Picker
    This patch sets up the google picker API for uploading/managing
    photos.

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,25 +2,82 @@ import React from 'react';
 import GoogleLogin from 'react-google-login';
 import config from '../config';
 
+// https://developers.google.com/api-client-library/javascript/start/start-js
+// https://developers.google.com/api-client-library/javascript/samples/samples#authorizing-and-making-authorized-requests
+
+// https://developers.google.com/picker/docs/
+
+// QUESTION
+// IS the token saved on the window now?
+// Setting visability of signin/signout on token
+// Checking for auth2
+
 export default class Login extends React.Component {
-  successGoogle(event) {
-    const storage = window.sessionStorage;
-    storage.setItem('tokenObj', JSON.stringify(event.tokenObj));
-    storage.setItem('profileObj', JSON.stringify(event.profileObj));
+  constructor(props) {
+    super(props);
+    // might need to move oauthToken to parent scope
+    this.state = {
+      auth2: null,
+      pickerApiLoaded: false,
+      oauthToken: null
+    };
   }
-  failureGoogle(event) {
-    console.log('failure because of: ', event)
+  componentDidMount () {
+    window.gapi.load('auth2', () => {
+      console.log('auth2 loaded')
+    })
+    window.gapi.load('picker', {'callback': this.onPickerApiLoad});
   }
+
+  onPickerApiLoad() {
+    console.log('onPickerApiLoad loaded')
+    this.pickerApiLoaded = true;
+    this.createPicker();
+  }
+
+    handleAuthResult(authResult) {
+      if (authResult && !authResult.error) {
+        this.oauthToken = authResult.access_token;
+        this.createPicker();
+      }
+    }
+
+    createPicker() {
+      console.log('createPicker clicked')
+      if (this.pickerApiLoaded && this.oauthToken) {
+        let picker = new google.picker.PickerBuilder().
+        addView(google.picker.ViewId.PHOTOS).
+        setOAuthToken(this.oauthToken).
+        setDeveloperKey(developerKey).
+        setCallback(pickerCallback).
+        build();
+
+    // set this visability later
+    picker.setVisible(true);
+    }
+  }
+    handleSignInClick(event) {
+      // might need to authorize with a developer key later...
+      window.gapi.auth2.authorize(
+          {
+            'client_id': config.ClientID,
+            'scope': ['https://www.googleapis.com/auth/photos'],
+            'immediate': false
+          },
+          this.handleAuthResult);
+    }
+
+    handleSignOutClick(event) {
+      window.gapi.auth2.getAuthInstance().signOut();
+    }
+
   render() {
     return (
       <div>
         <h1>Login Page</h1>
-        <GoogleLogin
-          clientId={config.GoogleAPIKey}
-          buttonText="Login"
-          onSuccess={this.successGoogle}
-          onFailure={this.failureGoogle}
-        />
+          <button id="signin-button" onClick={this.handleSignInClick.bind(this)}>Sign In</button>
+          <button id="signout-button" onClick={this.handleSignOutClick}>Sign Out</button>
+          <button id="signout-button" onClick={this.makeApiCall}>makeApiCall</button>
       </div>
     );
   }
