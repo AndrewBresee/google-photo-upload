040f60155633650501ba3ed1e339a2744b73deb8
Andrew Bresee <andrew.bresee@smartthings.com>
2017-09-17 17:37:54 -0700

    Restructure App so that login functionality is handled differently
    This patch adds the login/out handlers to the app level
    component and passes the props down to child components. This
    allows for the consistent rendering and rerending of child
    components depending on the logedIn state.

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,7 +4,6 @@ import { Router, Route, browserHistory, Redirect, IndexRoute } from 'react-route
 
 import App from './App';
 import Home from './Home';
-import Login from './Login';
 import Profile from './Profile';
 import Gallery from './Gallery';
 import Upload from './Upload';
@@ -17,7 +16,6 @@ import Upload from './Upload';
 const Root = () => (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={Login}/>
       <Route path="/home" component={Home} />
       <Route path="/profile" component={Profile} />
       <Route path="/upload" component={Upload} />
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,28 +1,94 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
+import config from '../config';
 
-import Navigation from './Navigation';
+// https://developers.google.com/picker/docs/
+// https://developers.google.com/picker/docs/reference#ViewId
 
 export default class Upload extends React.Component {
-  previewFile (event) {
-    const file = event.target.files[0]
-    const reader = new FileReader();
-
-    reader.onloadend = () => {
-      let targetElement = this.refs.imgPreview
-      targetElement.src = reader.result
+  constructor(props) {
+    super(props);
+    // might need to move oauthToken to parent scope
+    this.state = {
+      auth2ApiLoaded: null,
+      pickerApiLoaded: false,
+      logedIn: this.props.logedIn
     };
-
-    if (file) {
-      reader.readAsDataURL(file);
-    }
   }
 
+  componentWillReceiveProps(nextProps) {
+    console.log('Props are udated!!!')
+  }
+
+  pickerCallback(data) {
+     let url = 'nothing';
+     if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
+       console.log('data uploaded!: ', data)
+       let doc = data[google.picker.Response.DOCUMENTS][0];
+       url = doc[google.picker.Document.URL];
+     }
+     // a modal here instead?
+     let message = 'You picked: ' + url;
+     document.getElementById('result').innerHTML = message;
+   }
+
+    uploadPhoto() {
+      console.log('upload photo clicked')
+      let savedAuth = JSON.parse(localStorage.getItem('GoogleAuth'));
+      if (savedAuth.access_token) {
+        let picker = new google.picker.PickerBuilder().
+        addView(google.picker.ViewId.PHOTO_UPLOAD).
+        setOAuthToken(savedAuth.access_token).
+        setDeveloperKey(config.UploadAPIKey).
+        setCallback(this.pickerCallback).
+        build();
+
+        picker.setVisible(true);
+      } else {
+        console.error('No longer logged in')
+        // redirect
+      }
+    }
+
+    takePhoto() {
+      let savedAuth = JSON.parse(localStorage.getItem('GoogleAuth'));
+      if (savedAuth.access_token) {
+        let picker = new google.picker.PickerBuilder().
+        addView(google.picker.ViewId.WEBCAM).
+        setOAuthToken(savedAuth.access_token).
+        setDeveloperKey(config.UploadAPIKey).
+        setCallback(this.pickerCallback).
+        build();
+
+        picker.setVisible(true);
+      } else {
+        console.error('No longer logged in')
+        // redirect
+      }
+    }
+
   render() {
+    // TODO: Fix the way state is handled here. Loging in/out does not cause a rerender
+    let savedAuth = JSON.parse(localStorage.getItem('GoogleAuth'));
+    let logInOrOut = null;
+    if (savedAuth != null) {
+      logInOrOut = (
+        <div>
+          <button id="signout-button" onClick={this.uploadPhoto.bind(this)}>Upload Photo</button>
+          <button id="signout-button" onClick={this.takePhoto.bind(this)}>Take Photo</button>
+        </div>
+      )
+    } else {
+      logInOrOut = (
+        <div>
+          You are not logged in. Login to upload or take photos
+        </div>
+      )
+    }
     return (
       <div>
-        <h2>Upload your photos here</h2>
-        <input type="file" onChange={this.previewFile.bind(this)}/>
-        <img src="" ref="imgPreview"/>
+        <h1>Upload</h1>
+        {logInOrOut}
       </div>
     );
   }
