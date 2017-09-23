63dbc1eb3b283732c09ab04e685c106c6a41057b
Andrew Bresee <andrew.bresee@smartthings.com>
2017-09-23 11:09:40 -0700

    Add S3 route to upload an image to S3
    This patch create a route to upload an image to an S3 bucket, AND
    a specific folder. These specific folders will be used for each
    user.

--- scripts/server.js
+++ scripts/server.js
@@ -3,6 +3,18 @@ import webpack from 'webpack';
 import path from 'path';
 import config from '../webpack.config.dev';
 import open from 'open';
+import fs from 'fs';
+import S3FS from 's3fs';
+import configFile from '../src/config'
+let s3fsImpl = new S3FS('reactphotouploader', {
+  accessKeyId: configFile.AWSAccessKeyID,
+  secretAccessKey: configFile.AWSSecretKey
+});
+
+import multiparty from 'connect-multiparty';
+
+// file handling middleware
+let multipartyMiddleware = multiparty();
 
 /* eslint-disable no-console */
 
@@ -15,7 +27,24 @@ app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }));
 
+app.use(multipartyMiddleware);
 app.use(require('webpack-hot-middleware')(compiler));
+
+app.post('/testUpload', (req, res) => {
+  let file = req.files.image;
+  let folderName = req.body.folder
+  let stream = fs.createReadStream(file.path);
+  return s3fsImpl.writeFile(folderName + "\\" + file.originalFilename, stream).then(() => {
+    fs.unlink(file.path, (err) => {
+      if (err) {
+        console.error(err)
+      } else {
+        console.log('Upload worked!')
+      }
+    })
+    res.redirect('/gallery')
+  })
+})
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join( __dirname, '../src/index.html'));
