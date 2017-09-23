cc86bd0dd0f280207cb4f7491474a3dd8f1363cf
Andrew Bresee <andrew.bresee@smartthings.com>
2017-09-23 09:44:50 -0700

    End the use of Google Photos API and start switching to S3
    This is the last of the Google Photo API useage. This PR will be
    for reference in case I want to revert.
    
    Google Photos API had many deadends, and I am switching it out in
    place of Amazon S3.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -22,6 +22,7 @@ export default class Gallery extends React.Component {
               console.log('data: ', data)
               var photos = [];
               $.each(data.feed.entry, function(item){
+                  // TODO: Get the id of the albumb here and use that for uploading later
                   console.log('item: ', item)
                   photos.push({
                       src: this.content.src
@@ -36,6 +37,43 @@ export default class Gallery extends React.Component {
         .done(function(data){
             console.log(data);
         });
+  }
+  createAlbum() {
+    var request = '<?xml version="1.0" encoding="UTF-8"?>' +
+           '<entry xmlns="http://www.w3.org/2005/Atom"'> +
+           '     <title type="text">Trip To Italy</title>'+
+           '</entry>';
+
+   var request2 = '<entry xmlns="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:gphoto="http://schemas.google.com/photos/2007"><title type="text">Trip To Italy</title><summary type="text">This was the recent trip I took to Italy.</summary><gphoto:location>Italy</gphoto:location><gphoto:access>public</gphoto:access><gphoto:timestamp>1152255600000</gphoto:timestamp><media:group><media:keywords>italy, vacation</media:keywords></media:group><category  scheme="http://schemas.google.com/g/2005#kind" term="http://schemas.google.com/photos/2007#album"></category></entry>'
+
+    console.log('request2: ', request2)
+
+    let savedAuth = JSON.parse(localStorage.getItem('GoogleAuth'));
+    let access_token = savedAuth.Zi.access_token
+    let userId = "109317027548384374583"
+    $.ajax({
+      type: "POST",
+      contentType: "text/xml",
+      dataType: "jsonp",
+      url: 'https://picasaweb.google.com/data/feed/api/user/' + userId,
+      data: request2,
+      beforeSend: function(xhr){ //headers
+          xhr.setRequestHeader("Content-Type: application/xml");
+          xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+          xhr.setRequestHeader('GData-Version', '2');
+      },
+      processData: false,
+      success: function(data) {
+        console.log('success?')
+        let response = data;
+        let parsedData = $.parseXML(response);
+        console.log('POST success!: ', data)
+        console.log('parsedData: ', parsedData)
+      },
+      fail: function(){
+          console.log("fail");
+      }
+    });
   }
   getPhotos() {
     // change takazudo to the current user
@@ -68,6 +106,7 @@ export default class Gallery extends React.Component {
       <div>
         <h1>See your uploads</h1>
         <button onClick={this.getPicasa}>Get picasa</button>
+        <button onClick={this.createAlbum}>Create albumb</button>
       </div>
     );
   }
