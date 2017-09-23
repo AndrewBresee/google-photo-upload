887e8e68fe3ce7e5b64011cb740e85a25595a259
Andrew Bresee <andrew.bresee@smartthings.com>
2017-09-23 15:50:29 -0700

    Removed s3fs module in place of the AWS SDK
    This patch replaces the s3fs module with the more standard AWS
    SDK. This patch also adds the proper endpoints for a GET and POST
    for images.

--- scripts/server.js
+++ scripts/server.js
@@ -4,12 +4,14 @@ import path from 'path';
 import config from '../webpack.config.dev';
 import open from 'open';
 import fs from 'fs';
-import S3FS from 's3fs';
 import configFile from '../src/config'
-let s3fsImpl = new S3FS('reactphotouploader', {
-  accessKeyId: configFile.AWSAccessKeyID,
-  secretAccessKey: configFile.AWSSecretKey
-});
+
+// http://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/s3-example-photo-album.html#s3-example-photo-album-create-album
+
+
+import AWS from 'aws-sdk'
+let s3 = new AWS.S3();
+let params = {Bucket: 'reactphotouploader', Key: 'bresee/' }
 
 import multiparty from 'connect-multiparty';
 
@@ -33,18 +35,31 @@ app.use(require('webpack-hot-middleware')(compiler));
 app.post('/testUpload', (req, res) => {
   let file = req.files.image;
   let folderName = req.body.folder
-  let stream = fs.createReadStream(file.path);
-  return s3fsImpl.writeFile(folderName + "\\" + file.originalFilename, stream).then(() => {
-    fs.unlink(file.path, (err) => {
-      if (err) {
-        console.error(err)
-      } else {
-        console.log('Upload worked!')
-      }
-    })
-    res.redirect('/gallery')
-  })
-})
+  let fileName = file.name;
+  let albumPhotosKey = encodeURIComponent(folderName) + '//';
+
+  let photoKey = albumPhotosKey + fileName;
+  s3.upload({
+    Key: photoKey,
+    Body: file,
+    ACL: 'public-read'
+  }, function(err, data) {
+    if (err) {
+      console.log('There was an error uploading your photo: ', err.message);
+    }
+    console.log('Successfully uploaded photo.');
+  });
+});
+
+app.get('/getS3Folder', (req, res) => {
+  return s3.getObject(params, (err, data) => {
+    if (err) {
+      console.log(err, err.stack)
+    } else {
+      console.log(data);
+    }
+  });
+});
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join( __dirname, '../src/index.html'));
