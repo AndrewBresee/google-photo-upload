528962018bc2ad020c4046efb40c0559cd2bd56b
Andrew Bresee <andrew.bresee@smartthings.com>
2017-10-11 21:39:54 -0700

    Implement initial unit tests on Express Server
    This patch implements some initial tests using mocha and chai.
    It tests some endpoints on the server. Future tests will hit the
    frontend.

--- test/appTest.js
+++ test/appTest.js
@@ -1,3 +1,4 @@
+var should = require('should')
 var request = require('supertest')
 let chai = require('chai');
 var app = require('../scripts/server');
@@ -10,20 +11,24 @@ describe('badRouteTest', function() {
     });
 });
 
-describe('Get S3 Folder', function() {
-  this.timeout(15000);
-  it('Checks if S3 connection is working', function(done) {
-    request(app).get('/getS3Folder?folder=andrewbreseesmartthings')
-      .expect(200, done);
-    });
-});
-
 
 describe('Post to S3 Folder', function() {
     this.timeout(15000);
     it('Checks upload to S3 is working', function(done) {
        request(app).post('/upload')
-        .attach('file', './testUpload.png')
-        .expect(200, done);
+        .field('folder', 'testfolder')
+        .attach('uploadImage', __dirname + '/testUpload.png')
+        .expect(200)
+        .end(function(err, res1) {
+          res1.body.should.have.property('ETag')
+          done();
+        })
+    });
+});
+
+describe('Get S3 Folder', function() {
+  it('Checks if S3 connection is working', function(done) {
+    request(app).get('/getS3Folder?folder=testfolder')
+      .expect(200, done)
     });
 });
